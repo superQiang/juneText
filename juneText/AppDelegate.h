dd5f4f64b0a96f17a0b6363e13292e63fdace35f
superQiang <donghongqiangdhq@163.com>
2013-07-09 15:13:12 +0800

    Initial Commit

---
+++ juneText/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  juneText
+//
+//  Created by apple on 13-7-9.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
